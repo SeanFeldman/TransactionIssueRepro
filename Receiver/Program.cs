5cd7ae0c02b2a22b3143a01c0adfa837cd67bf8e
Sean Feldman <feldman.sean@gmail.com>
2019-02-14 22:06:24 -0700

    Add receive/send loop

--- Receiver/Program.cs
+++ Receiver/Program.cs
@@ -2,8 +2,10 @@
 
 namespace Sender
 {
+    using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
+    using System.Transactions;
     using Microsoft.Azure.ServiceBus;
     using Microsoft.Azure.ServiceBus.Core;
     using Microsoft.Azure.ServiceBus.Management;
@@ -32,18 +34,60 @@ namespace Sender
                 });
             }
 
-            var subscription = new SubscriptionDescription(Constants.TopicName, Constants.SubscriptionName)
+            if (!await client.SubscriptionExistsAsync(Constants.TopicName, Constants.SubscriptionName))
             {
-                EnableBatchedOperations = true,
-                MaxDeliveryCount = int.MaxValue,
-                ForwardTo = Constants.ReceiverQueueName
-            };
 
-            var rule = new RuleDescription(Constants.ReceiverQueueName, new SqlFilter("sys.Label LIKE 'FooEvent%'"));
+                var subscription = new SubscriptionDescription(Constants.TopicName, Constants.SubscriptionName)
+                {
+                    EnableBatchedOperations = true,
+                    MaxDeliveryCount = int.MaxValue,
+                    ForwardTo = Constants.ReceiverQueueName
+                };
 
-            await client.CreateSubscriptionAsync(subscription, rule);
+                var rule = new RuleDescription(Constants.ReceiverQueueName,
+                    new SqlFilter("sys.Label LIKE 'FooEvent%'"));
+
+                await client.CreateSubscriptionAsync(subscription, rule);
+            }
 
             await client.CloseAsync();
+
+            var connection = new ServiceBusConnection(connectionString);
+            var receiver = new MessageReceiver(connection, Constants.ReceiverQueueName, ReceiveMode.PeekLock);
+            receiver.PrefetchCount = 0;
+            var sender = new MessageSender(connection, Constants.TopicName, Constants.ReceiverQueueName);
+
+            while (true)
+            {
+                var incoming = await receiver.ReceiveAsync();
+
+                //log.Info($"Received FooEvent with ID {incoming.MessageId}, sleeping 30ms to simulate normal usage");
+
+                using (var tx = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+                {
+
+                    await Task.Delay(30);
+
+                    var numberOfEventsToPublish = 10;
+
+                    var events = Enumerable.Range(1, numberOfEventsToPublish).Select(x => new Message
+                    {
+                        MessageId = Guid.NewGuid().ToString(),
+                        Label = $"BarEvent #{x}"
+                    });
+
+                    var tasks = new List<Task>(numberOfEventsToPublish);
+                    tasks.AddRange(events.Select(@event => sender.SendAsync(@event)));
+
+                    await Task.WhenAll(tasks).ConfigureAwait(false);
+
+                    await receiver.CompleteAsync(incoming.SystemProperties.LockToken);
+
+                    tx.Complete();
+
+                    //log.Info($"Published {numberOfEventsToPublish} BarEvent events.");
+                }
+            }
         }
     }
 
