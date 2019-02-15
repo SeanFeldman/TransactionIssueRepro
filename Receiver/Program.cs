449ee97be667dfe87c0c779f68d426b6e509270a
Sean Feldman <feldman.sean@gmail.com>
2019-02-14 21:56:46 -0700

    Infrastracture for Receiver

---
+++ Receiver/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Sender
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.ServiceBus;
+    using Microsoft.Azure.ServiceBus.Core;
+    using Microsoft.Azure.ServiceBus.Management;
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("AzureServiceBus_ConnectionString");
+
+            var client = new ManagementClient(connectionString);
+
+            if (!await client.TopicExistsAsync(Constants.TopicName))
+            {
+                throw new Exception($"Topic '{Constants.TopicName}' should be created first and seeded.");
+            }
+
+            if (!await client.QueueExistsAsync(Constants.ReceiverQueueName))
+            {
+                await client.CreateQueueAsync(new QueueDescription(Constants.ReceiverQueueName)
+                {
+                    MaxDeliveryCount = int.MaxValue,
+                    LockDuration = TimeSpan.FromMinutes(5),
+                    MaxSizeInMB = 5 * 1024,
+                    EnableBatchedOperations = true
+                });
+            }
+
+            var subscription = new SubscriptionDescription(Constants.TopicName, Constants.SubscriptionName)
+            {
+                EnableBatchedOperations = true,
+                MaxDeliveryCount = int.MaxValue,
+                ForwardTo = Constants.ReceiverQueueName
+            };
+
+            var rule = new RuleDescription(Constants.ReceiverQueueName, new SqlFilter("sys.Label LIKE 'FooEvent%'"));
+
+            await client.CreateSubscriptionAsync(subscription, rule);
+
+            await client.CloseAsync();
+        }
+    }
+
+}
--- Sender/Constants.cs
+++ Sender/Constants.cs
@@ -1,5 +1,6 @@
 ﻿public class Constants
 {
     public const string TopicName = "bundle-1";
+    public const string SubscriptionName = "receiver";
     public const string ReceiverQueueName = "receiver";
 }
